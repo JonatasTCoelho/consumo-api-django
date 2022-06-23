a01ccf9a6f0690816c9bc8287fbd91abe4a92a81
jonatas Tonin <jonatascoelho9@gmail.com.br>
2022-06-22 22:17:12 -0300

    changes

--- feriado/modules/views.py
+++ feriado/modules/views.py
@@ -1,22 +1,22 @@
-from datetime import datetime
-from django.shortcuts import render
-from django.http import HttpResponse
-import requests
+
+from django.shortcuts import render, HttpResponse 
+from datetime import date
+
+from .models import FeriadoModel
+
 
 def holliday(request):
-  requisicao = requests.get("https://brasilapi.com.br/api/feriados/v1/2022")
-  try:
-        listFeriado = requisicao.json()
-  except ValueError:
-        print("A resposta não chegou com o formato esperado.")
-
-  dateHollidays = []
-  hoje = datetime.today().strftime('%Y-%m-%d')
-  for valor in listFeriado:
-        dateHollidays.append(valor['date'])
-
-  return HttpResponse(dateHollidays)
-
-  return render(request, "index.html", contexto)
+    hoje = date.today()
+    
+    if FeriadoModel.objects.filter(ano=hoje.year, mes=hoje.month, dia=hoje.day).exists():
+        contexto = {
+            'ehferiado': True,
+        }
+    else:
+        contexto = {
+            'ehferiado': False,
+        }
+    
+    return render(request, 'index.html', contexto)
 
 # Create your views here.
